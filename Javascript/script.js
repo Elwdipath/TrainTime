ed2fcf9f46a9bc405f22f7c1125db0e77eb64d9b
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-02-04 13:41:06 -0500

    took measure to hide API key

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,20 +1,46 @@
 // FINISH HTML!
-const firebaseConfig = {
-    apiKey: "AIzaSyBoU4QEHT0R5SGhL4VQy8aW4sEo7SXliX0",
-    authDomain: "train-time-da527.firebaseapp.com",
-    databaseURL: "https://train-time-da527.firebaseio.com",
-    projectId: "train-time-da527",
-    storageBucket: "train-time-da527.appspot.com",
-    messagingSenderId: "920741048353",
-    appId: "1:920741048353:web:2d80729f1739573f9bb360"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
-  let db = firebase.database();
+// $(document).ready(function() {
 
-  let tN;
-  let tD;
-  let tF;
-  let nA;
-  let mA;
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyBoU4QEHT0R5SGhL4VQy8aW4sEo7SXliX0",
+  //   authDomain: "train-time-da527.firebaseapp.com",
+  //   databaseURL: "https://train-time-da527.firebaseio.com",
+  //   projectId: "train-time-da527",
+  //   storageBucket: "train-time-da527.appspot.com",
+  //   messagingSenderId: "920741048353",
+  //   appId: "1:920741048353:web:ccd28e555e74f5fd9bb360"
+  // }
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    let db = firebase.database();
+
+    let tN;
+    let tD;
+    let tF;
+    let nA;
+    let mA;
+    let fT
+
+    //Capture data from submit button and push to db
+
+    $("#subB").on("click", function(event){
+        event.preventDefault();
+        
+        alert("Here")
+        tN = $("#name-input").val().trim();
+        tD = $("#destination-input").val().trim();
+        tF = $("#frequency-input").val().trim();
+        fT = $("first-input").val().trim();
+
+        db.ref().push({
+          trainName: tN,
+          destination: tD,
+          frequency: tF,
+          firstTrain: fT,
+        })
+        
+        
+    })
+  // })
