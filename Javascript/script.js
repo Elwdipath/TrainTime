c44fc0edc2f61872a160cfd1f52563600c757fdb
Jordan <jordan.kaiser@gmail.com>
2020-02-03 21:41:55 -0500

    added HTML form and firebase

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,14 @@
 // FINISH HTML!
+const firebaseConfig = {
+    apiKey: "AIzaSyBoU4QEHT0R5SGhL4VQy8aW4sEo7SXliX0",
+    authDomain: "train-time-da527.firebaseapp.com",
+    databaseURL: "https://train-time-da527.firebaseio.com",
+    projectId: "train-time-da527",
+    storageBucket: "train-time-da527.appspot.com",
+    messagingSenderId: "920741048353",
+    appId: "1:920741048353:web:2d80729f1739573f9bb360"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
