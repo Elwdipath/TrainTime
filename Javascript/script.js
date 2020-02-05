cacdd358e3acb85a229aceef26b3ae4758c4f52d
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-02-05 13:21:25 -0500

    finished assignment

--- Javascript/script.js
+++ Javascript/script.js
@@ -21,26 +21,67 @@
     let tF;
     let nA;
     let mA;
-    let fT
+    var fT;
 
     //Capture data from submit button and push to db
 
     $("#subB").on("click", function(event){
         event.preventDefault();
         
-        alert("Here")
+        
         tN = $("#name-input").val().trim();
         tD = $("#destination-input").val().trim();
+        fT = $("#first-train-time-input").val().trim();
         tF = $("#frequency-input").val().trim();
-        fT = $("first-input").val()
-
+        
+        console.log(fT)
         db.ref().push({
           trainName: tN,
           destination: tD,
           frequency: tF,
           firstTrain: fT,
         })
-        
-        
-    })
-  // })
+        $("#name-input").val(' ')
+        $("#destination-input").val(' ')
+        $("#frequency-input").val(' ')
+        $("#first-train-time-input").val(' ')
+  })
+
+  db.ref().on("child_added", function(snapshot){
+      var dv = snapshot.val()
+
+      var nTrain = dv.trainName;
+      var tDest = dv.destination;
+      var tFreq = dv.frequency;
+      var fTrain = dv.firstTrain;
+
+      var firstTimeConverted = moment(fTrain, "HH:mm").subtract(1, "years")
+        console.log("First Time: " + firstTimeConverted)
+      
+      //Current Time
+
+      var cTime = moment();
+        console.log(cTime)
+
+      //Difference between times
+
+      var dTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("Time Difference: " + dTime)
+
+      // Divide out to get remainder
+
+      var tRemainder = dTime % tFreq  
+        console.log ("Remainder: " + tRemainder);
+
+      // Minute Until Train
+    var tMinus = tFreq - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinus);
+
+      // Next Train
+
+    var nextTrain = moment().add(tMinus, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+
+        $("#tSchedule").append("<tr><td>" + nTrain + "</td><td>" + tDest + "</td><td>" + tFreq + 
+        "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinus + "</td></tr>");
+  })
